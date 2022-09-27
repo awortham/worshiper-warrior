aae556166d78fee10f1735ecddf7a7e5296df0e7
Aaron Wortham <wortham.aaron@gmail.com>
2022-09-26 20:22:39 -0600

    test resource

---
+++ app/avo/resources/post_resource.rb
@@ -0,0 +1,12 @@
+class PostResource < Avo::BaseResource
+  self.title = :id
+  self.includes = []
+  # self.search_query = -> do
+  #   scope.ransack(id_eq: params[:q], m: "or").result(distinct: false)
+  # end
+
+  field :id, as: :id
+  field :title, as: :text
+  field :body, as: :trix
+  # add fields here
+end
